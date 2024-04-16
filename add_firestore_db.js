98267e486c1a9b5acc86b8f3215e42bbe973b1b0
kietdev123 <79886528+kietdev123@users.noreply.github.com>
2024-04-16 15:16:10 +0700

    feat: add data to firestore database

---
+++ add_firestore_db.js
@@ -0,0 +1,108 @@
+const { initializeApp } = require('firebase/app');
+const fs = require("fs");
+const {
+    getFirestore,
+    collection,
+    doc,
+    addDoc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+  } = require('firebase/firestore');
+const firebase = initializeApp({
+    apiKey: process.env.apiKey,
+    authDomain:  process.env.authDomain,
+    projectId:  process.env.projectId,
+    storageBucket:  process.env.storageBucket,
+    messagingSenderId:  process.env.messagingSenderId,
+    appId:  process.env.appId,
+});
+
+const db = getFirestore(firebase);
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const brands = [
+    "adidas",
+    "adidas Originals",
+    "Blend",
+    "Boutique Moschino",
+    "Champion",
+    "Diesel",
+    "Jack & Jones",
+    "Naf Naf",
+    "Red Valentino",
+];
+
+const sizes = [
+    "S", "M", "L"
+];
+
+async function seedDataBrand(){
+    try {
+        await addDoc(collection(db, 'brand'), {'names' : brands} );
+        console.log('Done added brand');
+    } catch (e){
+        console.log(e);
+    }
+
+}
+
+async function seedDataType(){
+    fs.readFile("./type.json", "utf8", async (error, data) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        let value = JSON.parse(data)
+        await addDoc(collection(db, 'product_type'), value);
+        console.log('Done added product type');
+    })
+}
+
+async function seedDataProduct(){
+    try {
+        let products = await fs.readFileSync("./products.json", 'utf8');
+        products = JSON.parse(products)['data'];
+
+        let product_images = await fs.readFileSync("./product_images.json", 'utf8');
+        product_images = JSON.parse(product_images)['data'];
+
+        for (let i in products){
+           
+            let value = products[i];
+         
+            value.imageUrl = product_images[i].url;
+            value.price = getRandomInt(100000, 1000000);
+            value.saleNumber = getRandomInt(0, 100);
+            value.star = getRandomInt(0, 5);
+  
+            let sizeIndex =  getRandomInt(0, 2);
+
+            value.size = sizes[sizeIndex];
+            
+            let brandIndex =  getRandomInt(0, 8);
+            value.brand = brands[brandIndex];
+      
+            await addDoc(collection(db, 'products'), value );
+        }
+        console.log('Done added product');
+    } catch (e){
+        console.log(e);
+    }  
+}
+
+
+
+async function main(){
+    // await seedDataBrand();
+    // await seedDataType();
+    await seedDataProduct();
+}
+
+main();
