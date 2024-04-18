015a65d7eb4bcf8706532bd1addb13a6dd7beb57
kietdev123 <79886528+kietdev123@users.noreply.github.com>
2024-04-18 15:56:27 +0700

    feat: crawl images data

--- README.md
+++ README.md
@@ -3,26 +3,19 @@
 ## Description
 
 use to create type, products in Ecommerce fashion for personal project
+Download data from [Fashion product from Kaggle](https://www.kaggle.com/datasets/paramaggarwal/fashion-product-images-dataset/code)
 
 ## Usage
 
-Step 1: Download data from [Fashion product from Kaggle](https://www.kaggle.com/datasets/paramaggarwal/fashion-product-images-dataset/code)
-
-Step 2: Create data foulder with images - foulder and styles.csv
-
-Step 3: Create type.json from styles.csv, run
+Step 1: Create type.json from styles.csv, run
 
     py get_type_json.py
 
-Step 4: Create products.json from styles.csv, run
+Step 2: Create products.json from styles.csv, run
 
     py get_products_json.py
 
-Step 5: Create .env with .env template, get product_images.json when upload images to cloudinary, run
-
-    node --env-file=.env upload_images.js
-
-Step 6: add data to firestore database
+Step 3: Create .env with .env template, add data to firestore database
 
     node --env-file=.env add_firestore_db.js
 
--- get_products_json.py
+++ get_products_json.py
@@ -1,7 +1,33 @@
-
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+import time
 import pandas as pd
 import csv
 import json
+
+driver = webdriver.Chrome() 
+
+def searchImages(searchKey):
+    #  "Men Footwear Sandal summer"
+    driver.get("https://www.google.com/search?q=" + searchKey )
+
+    aResults = driver.find_elements(By.XPATH,"//div[@class='crJ18e']//div//a")
+    # time.sleep(1)
+    # click to shopping tag
+    aResults[1].click()
+
+    imgResults = driver.find_elements(By.XPATH,"//div[@class='ArOc1c']//img")
+
+    i = 0
+    strs = []
+    for img in imgResults:
+        str = img.get_attribute('src')
+        strs.append(str)
+        # print(str)
+        i += 1
+        if (i == 3): break; 
+    return strs
+
 
 file = open("data/styles.csv")
 
@@ -17,10 +43,33 @@ for row in csvreader:
 
 file.close()
 
-dictionary = {
-    "data": rows,
-}
- 
-# Writing to sample.json
-with open("products.json", "w") as outfile:
-    json.dump(dictionary, outfile)
+count = 0
+for i in rows:
+    while (1):
+       try:
+         i["imgUrls"] = searchImages(
+            i["gender"] + " " +
+            i["masterCategory"] +  " " +
+            i["subCategory"] + " " +
+            i["articleType"] +  " " +
+            i["baseColour"] + " " +
+            i["season"] +  " " +
+            i["usage"] +  " " 
+            )
+         break
+       except:
+        print("An exception occurred")
+  
+    count +=1
+    print("Crawled images for product ",count)
+    
+    dictionary = {
+        "data": rows,
+    }
+    
+    # Writing to sample.json
+    with open("products.json", "w") as outfile:
+        json.dump(dictionary, outfile)
+
+driver.quit()
+print("Done")
